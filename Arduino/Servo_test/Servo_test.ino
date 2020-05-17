bafe8125164c998fa0c1792c4261f75d3fb82097
Caprasilana <caprasilana@gmail.com>
2020-05-17 10:53:55 +0200

    Arduino Servo test fix

--- Arduino/Servo_test/Servo_test.ino
+++ Arduino/Servo_test/Servo_test.ino
@@ -17,24 +17,24 @@ int val;    // variable to read the value from the analog pin
 
 void setup() {
   // attaches the servo on pin 9 to the servo object
-  servo.attach(PIN_SERVO);  /
+  servo.attach(PIN_SERVO);  
 }
 
 void loop() {
   // move to pos 0
-  myservo.write(0); 
+  servo.write(0); 
   // wait 1 sec
   delay(1000); 
   // move to pos 45
-  myservo.write(45); 
+  servo.write(45); 
   // wait 1 sec
   delay(1000);
   // move to pos 90
-  myservo.write(90); 
+  servo.write(90); 
   // wait 1 sec
   delay(1000);
   // move to pos 180
-  myservo.write(180); 
+  servo.write(180); 
   // wait 1 sec
   delay(1000);                         
 }
